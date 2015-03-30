036759b6b243ca924cac906aeb3f57135e6f5ee3
gaohailang <ghlndsl@126.com>
2015-03-30 20:28:11 +0800

    chore: update some style and 2x retina images etc

--- modules/sites.js
+++ modules/sites.js
@@ -1,6 +1,7 @@
 // used for android snaptube video sites page
 var request = require('request');
 var cache = require('memory-cache');
+var _ = require('lodash');
 
 var CACHEKEY = 'androidSitesList';
 
@@ -16,6 +17,12 @@ module.exports = exports = {
         } else {
             request.get('http://www.wandoujia.com/needle/source/getJSON/59', function(err, resp, body) {
                 var data = JSON.parse(body);
+                // check retina media
+                _.each(data, function(i) {
+                    _.each(i.sites, function(ii) {
+                        ii.icon = ii.icon.split('.png')[0]+'@2x.png';
+                    });
+                });
                 cache.put(CACHEKEY, data, 1000 * 60 * 60);
                 render(data);
             });
