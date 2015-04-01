03623ae6c365adf931e22fd75a850f995eef677e
gaohailang <ghlndsl@126.com>
2015-04-01 18:35:36 +0800

    feat: remove cache for json config of sites~

--- modules/sites.js
+++ modules/sites.js
@@ -12,21 +12,23 @@ module.exports = exports = {
                 $data: data
             });
         }
-        if (cache.get(CACHEKEY)) {
+
+        request.get('http://www.wandoujia.com/needle/source/getJSON/59', function(err, resp, body) {
+            var data = JSON.parse(body);
+            // check retina media
+            _.each(data, function(i) {
+                _.each(i.sites, function(ii) {
+                    ii.icon = ii.icon.split('.png')[0] + '@2x.png';
+                });
+            });
+            // cache.put(CACHEKEY, data, 1000 * 60 * 60);
+            render(data);
+        });
+
+        /*if (cache.get(CACHEKEY)) {
             render(cache.get(CACHEKEY));
         } else {
-            request.get('http://www.wandoujia.com/needle/source/getJSON/59', function(err, resp, body) {
-                var data = JSON.parse(body);
-                // check retina media
-                _.each(data, function(i) {
-                    _.each(i.sites, function(ii) {
-                        ii.icon = ii.icon.split('.png')[0]+'@2x.png';
-                    });
-                });
-                cache.put(CACHEKEY, data, 1000 * 60 * 60);
-                render(data);
-            });
-        }
+        }*/
     },
     clear: function(req, res) {
         cache.del(CACHEKEY);
