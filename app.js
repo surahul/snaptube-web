a0fadfee58b1bb15ebc74889919139f58f0e922e
gaohailang <ghlndsl@126.com>
2015-04-16 16:16:00 +0800

    feat: add oathkeeper which avoid credential info leak~

--- app.js
+++ app.js
@@ -103,4 +103,8 @@ app.use(function(err, req, res, next) {
     console.log('uncaughtException caught the error');
 });*/
 
-app.listen(3000);
+var server = app.listen(3000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+});
